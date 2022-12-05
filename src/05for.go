6cb3be713c2476447233a5694350b4c9d447355b
bong <mcjoi@naver.com>
2022-12-05 11:56:17 +0900

    20221205

--- src/05for.go
+++ src/05for.go
@@ -1,38 +1,31 @@
-// package main
-
-
+package main
 
 import (
 	//"reflect" // typeof
-	"fmt" 
+	"fmt"
 	// "math"
 )
 
-
-
 func main() {
-	
-	
+
 	fmt.Println("#1 example")
 	i := 1
 	for i <= 3 {
 		fmt.Println(i)
 		i = i + 1
 	}
-	
+
 	fmt.Println("#2 example")
-	for j:=7; j <=9; j++ {
+	for j := 7; j <= 9; j++ {
 		fmt.Println(j)
 	}
-	
+
 	fmt.Println("#3 example")
 	for n := 3; n <= 10; n++ {
 		if n%2 == 0 {
-			 continue // or use break
+			continue // or use break
 		}
 		fmt.Println(n)
 	}
-	
-	
-	
+
 }
--- src/06if.go
+++ src/06if.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	//"reflect" // typeof
-	"fmt" 
+	"fmt"
 	// "math"
 )
 
 func main() {
-	
+
 	// else
 	if 7%2 == 0 {
-        fmt.Println("7 is even")
+		fmt.Println("7 is even")
 	} else {
-		fmt.Println("7 is odd")		
-	}	
-	
+		fmt.Println("7 is odd")
+	}
+
 	// without else
 	if 8%4 == 0 {
 		fmt.Println("8 is divided by 4")
 	}
-	
-	
+
 	if num := 90; num < 0 {
 		fmt.Println(num, "is negative")
-	}else if num < 10 {
+	} else if num < 10 {
 		fmt.Println(num, "has 1 digit")
-	}else {
+	} else {
 		fmt.Println(num, "has multiple digits")
 	}
-		
+
 	
+
 }

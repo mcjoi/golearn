0afac702a66ad5fc2893ea6bfa2ca1389534fac1
bong <mcjoi@naver.com>
2022-12-06 09:37:39 +0900

    20221206

--- src/21interfaces.go
+++ src/21interfaces.go
@@ -2,49 +2,36 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type geometry interface {
-	area() float64
-	perim() float64
+type personInterface interface {
+	runPrintName() string
+	runPrintAge() int
 }
 
-type rect struct {
-	width, height float64
+type person struct {
+	name string
+	age  int
 }
 
-func (r rect) area() float64 {
-	return r.width * r.height
+func (txt person) runPrintName() string {
+	return txt.name
+}
+func (txt person) runPrintAge() int {
+	return txt.age
 }
 
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
-}
+func runTest(p personInterface) {
 
-type circle struct {
-	radius float64
-}
-
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func measure(g geometry) {
-
-	fmt.Println(g)
-	fmt.Println(g.area())
-	fmt.Println(g.perim())
+	fmt.Println(p)
+	fmt.Println(p.runPrintName())
+	fmt.Println(p.runPrintAge())
 
 }
 
 func main() {
-	r := rect{width: 3, height: 4}
-	c := circle{radius: 5}
-	measure(r)
-	measure(c)
+
+	p1 := person{"myName", 19}
+	runTest(p1)
+
 }
